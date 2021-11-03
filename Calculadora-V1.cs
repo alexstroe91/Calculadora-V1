ba5b824e1593517fa59c625cba8ec1c0ac1d55a5
alexstroe91 <alexstroe91@gmail.com>
2021-11-03 10:11:54 +0100

    Termiada la calculadora y capturadas las excepciones necesarias.

---
+++ Calculadora-V1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class calculadora1
+    {
+        static int Main(string[] args)
+        {
+
+            String operacion = args[0];
+
+            try
+            {
+                if (args.Length < 3)
+                {
+                    throw new Exception($"--Error-- : No has introducido todos los parametros requeridos (3) . Parametros introducidos: ({args.Length})");
+                }
+                else if (args.Length > 3)
+                {
+                    throw new Exception($"--Error-- : Has introducido más argumentos que los necesarios (3). Parametros introducidos: ({args.Length})");
+                }
+                else
+                {
+                    switch (operacion)
+                    {
+                        case "--add":
+                            Console.WriteLine($"{args[1]} + {args[2]} = {decimal.Parse(args[1]) + decimal.Parse(args[2])}");
+                            break;
+                        case "--sub":
+                            Console.WriteLine($"{args[1]} - {args[2]} = {decimal.Parse(args[1]) - decimal.Parse(args[2])}");
+                            break;
+                        case "--div":
+
+                            if (decimal.Parse(args[2]) == 0)
+                            {
+                                throw new Exception("--Error-- : El divisor no puede ser 0");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{args[1]} / {args[2]} = {decimal.Parse(args[1]) / decimal.Parse(args[2])}");
+                            }
+
+                            break;
+                        case "--mul":
+                            Console.WriteLine($"{args[1]} x {args[2]} = {decimal.Parse(args[1]) * decimal.Parse(args[2])}");
+                            break;
+                        default:
+                            throw new Exception($"--Error-- : Ninguna de los argumentos indicados es correcto: {args[1]} ");
+                            break;
+
+                    }
+                    return 0;
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
