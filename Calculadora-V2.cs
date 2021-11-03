237cfd66933c2becfe23e8d4270a8091f79c16ea
alexstroe91 <alexstroe91@gmail.com>
2021-11-03 10:35:13 +0100

    Añadido la clase de CalculadoraV2

---
+++ Calculadora-V2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculadora.Process
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+
+
+
+
+            return 0;
+        }
+    }
+}
