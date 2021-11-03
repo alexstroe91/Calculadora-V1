f1c9cbd89642e9a63a95f8e93ba6ece6905ddfef
alexstroe91 <alexstroe91@gmail.com>
2021-11-03 10:25:29 +0100

    Comentado el código explicando la ejecucion

--- Calculadora-V1.cs
+++ Calculadora-V1.cs
@@ -6,23 +6,26 @@ namespace Calculadora
     {
         static int Main(string[] args)
         {
-
-            String operacion = args[0];
-
+            //Comienzo del bloque try catch
             try
             {
+                //compruebo la cantidad de argumentos que se introducen
                 if (args.Length < 3)
                 {
+                    //en el caso de que introduzca menos arugmentos de los necesarios lanzo la excepcion siguiente.
                     throw new Exception($"--Error-- : No has introducido todos los parametros requeridos (3) . Parametros introducidos: ({args.Length})");
                 }
                 else if (args.Length > 3)
                 {
+                    //en el caso de que introduzca mas arugmentos de los necesarios lanzo la excepcion siguiente.
                     throw new Exception($"--Error-- : Has introducido más argumentos que los necesarios (3). Parametros introducidos: ({args.Length})");
                 }
                 else
                 {
-                    switch (operacion)
+                    //en el caso de que los argumentos sean los que necesito, comienza el bloque de ejecución.
+                    switch (args[0])
                     {
+                        //realizo las operaciones necesarias dependiendo de que operación elige el usuario
                         case "--add":
                             Console.WriteLine($"{args[1]} + {args[2]} = {decimal.Parse(args[1]) + decimal.Parse(args[2])}");
                             break;
@@ -30,13 +33,14 @@ namespace Calculadora
                             Console.WriteLine($"{args[1]} - {args[2]} = {decimal.Parse(args[1]) - decimal.Parse(args[2])}");
                             break;
                         case "--div":
-
+                            //en el caso en el que el divisor es 0, como esa operacion no puede realizarse, lanzo la excepcion necesaria
                             if (decimal.Parse(args[2]) == 0)
                             {
                                 throw new Exception("--Error-- : El divisor no puede ser 0");
                             }
                             else
                             {
+                                //en el caso de que no, realizo la operacion
                                 Console.WriteLine($"{args[1]} / {args[2]} = {decimal.Parse(args[1]) / decimal.Parse(args[2])}");
                             }
 
@@ -45,14 +49,17 @@ namespace Calculadora
                             Console.WriteLine($"{args[1]} x {args[2]} = {decimal.Parse(args[1]) * decimal.Parse(args[2])}");
                             break;
                         default:
+                            //en el caso que el usuario no introduzca los argumentos correctamente, se lanza otra excepcion
                             throw new Exception($"--Error-- : Ninguna de los argumentos indicados es correcto: {args[1]} ");
                             break;
 
                     }
+                    // después de realizar las operaciones, se retorna 0, por que el código se ha realizado correctamente.
                     return 0;
                 }
             } catch (Exception ex)
             {
+                //se capturan las excepciones que se han lanzado durante la ejecución del código
                 Console.WriteLine(ex.Message);
                 return 1;
             }
