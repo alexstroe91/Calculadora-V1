00f40c07560cad37e204d9414a267cc2e28f968a
alexstroe91 <alexstroe91@gmail.com>
2021-11-09 14:00:43 +0100

    Hecha la referencia al proceso hijo. Terminado el metodo de sumar

--- Calculadora.Process/Calculadora-V2.cs
+++ Calculadora.Process/Calculadora-V2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace Calculadora.Process
+namespace Calculadora
 {
     class Program
     {
@@ -8,8 +9,44 @@ namespace Calculadora.Process
         {
             Console.WriteLine(args.Length);
 
+            Console.WriteLine("¿Qué operación quiere realizar?");
+            Console.Write("(Suma - s / Resta - r / Multiplicacion - m / Divison - d): ");
+            String operacion = Console.ReadLine();
+
+            decimal num1 = 0;
+            decimal num2 = 0;
+            string argumentoOperacion = "";
 
 
+            switch (operacion)
+            {
+                case "s":
+
+                    Console.Write("Introduce valor para numero1 : ");
+                    num1 = Decimal.Parse(Console.ReadLine());
+                    Console.Write("Introduce valor para numero2 : ");
+                    num2 = Decimal.Parse(Console.ReadLine());
+                    argumentoOperacion = "--add";
+
+
+                    ProcessStartInfo startInfo = new ProcessStartInfo(@"F:\DAM2A\Programacion de Servicios y Procesos\PracticaCalculadora\Calculadora\Calculadora\bin\Debug\net5.0\Calculadora.exe");
+                    startInfo.Arguments = $"{argumentoOperacion} {num1} {num2}";
+                    Process.Start(startInfo);
+
+
+                    break;
+
+                case "r":
+
+                    break;
+
+                default:
+                    //ProcessStartInfo startInfo = new ProcessStartInfo(@"F:\DAM2A\Programacion de Servicios y Procesos\PracticaCalculadora\Calculadora\Calculadora\bin\Debug\net5.0\Calculadora.exe");
+                    //startInfo.Arguments = $"{argumentoOperacion} {num1} {num2}";
+                    //Process.Start(startInfo);
+                    break;
+
+            }
 
             return 0;
         }
